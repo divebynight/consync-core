0203482e90ccd084c9bf058a42fa1831380d2dfa
divebynight <divebynight@revmecha.com>
2026-04-27 16:45:53 -0500

    Define export boundary and fix renderer packaging

--- vite.renderer.config.mjs
+++ vite.renderer.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   root: 'src/electron/renderer',
+  build: {
+    outDir: path.resolve('.vite/renderer/main_window'),
+  },
   plugins: [react()],
 });
